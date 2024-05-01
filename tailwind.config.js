6feae518361934ae810c793b73a555f6c11953cb
dassomen <somend265@gmail.com>
2024-05-01 16:04:04 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        sm:"0.75rem",
+        base:"0.875rem",
+        md:"1rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem"
+      },
+
+      backgroundImage: {
+        'team-bg-image': "url('/public/teams/team.png')",
+        'team-avatar': "url('/public/teams/team_avatar.png')",
+        
+      }
+    },
   },
   plugins: [],
 }
+
 
